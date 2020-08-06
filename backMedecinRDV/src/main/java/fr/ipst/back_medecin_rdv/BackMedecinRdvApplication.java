726a67ee0f7f4a36d457222478eecbf4c8390adf
Yanis <yanis.batatia@gmail.com>
2020-08-06 20:55:10 +0200

    premier commit du projet back

---
+++ backMedecinRDV/src/main/java/fr/ipst/back_medecin_rdv/BackMedecinRdvApplication.java
@@ -0,0 +1,13 @@
+package fr.ipst.back_medecin_rdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMedecinRdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackMedecinRdvApplication.class, args);
+    }
+
+}
---
+++ backMedecinRDV/src/test/java/fr/ipst/back_medecin_rdv/BackMedecinRdvApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ipst.back_medecin_rdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackMedecinRdvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
