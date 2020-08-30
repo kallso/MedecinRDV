249cbd7ab86df2b092ad69efc8204d0fba5767d2
Kevin C <costeskevin31@gmail.com>
2020-08-30 17:32:21 +0200

    [feat] Header - Footer : creation + ajout images

--- frontMedecinRDV/src/app/app.module.ts
+++ frontMedecinRDV/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
