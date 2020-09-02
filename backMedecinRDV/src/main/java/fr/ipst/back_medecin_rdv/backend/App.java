e03880b37b164825c87a3d80f519a08d3d679d7b
Yanis <yanis.batatia@gmail.com>
2020-09-02 16:52:38 +0200

    [BACK][IMP] corection pom.xml rendu controller fonctionnels

--- backMedecinRDV/src/main/java/fr/ipst/back_medecin_rdv/backend/App.java
+++ backMedecinRDV/src/main/java/fr/ipst/back_medecin_rdv/backend/App.java
@@ -10,8 +10,11 @@ import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFa
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+@ComponentScan(basePackages= {"fr.ipst.back_medecin_rdv.exceptions", "fr.ipst.back_medecin_rdv.controller", "fr.ipst.back_medecin_rdv.mapper", "fr.ipst.back_medecin_rdv.services", "fr.ipst.back_medecin_rdv.dto", "fr.ipst.back_medecin_rdv.backend"})
+@EntityScan("fr.ipst.back_medecin_rdv.entities")
+@EnableJpaRepositories(basePackages = "fr.ipst.back_medecin_rdv.repository")
 
+public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
