e0855e4c1cd1b8723b9196f5b4a8dff9efe37b7b
SorelRouge <kevin.astier88@gmail.com>
2020-09-02 17:27:18 +0200

    [front] formulaire complété

--- frontMedecinRDV/src/app/register/form-medecin/form-medecin.component.ts
+++ frontMedecinRDV/src/app/register/form-medecin/form-medecin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form-medecin',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-medecin.component.css']
 })
 export class FormMedecinComponent implements OnInit {
+  formMedecin: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.formMedecin = new FormGroup({
+      nom: new FormControl(),
+      prenom: new FormControl(),
+      telephone: new FormControl(),
+      codePostal: new FormControl(), 
+      adresse: new FormControl(),
+      ville: new FormControl(),
+      specialisation: new FormControl(),
+      mail: new FormControl(),
+      tempsRdv: new FormControl(),
+      motDePasse: new FormControl(),
+    })
+  }
+  onSubmit() {
+    return null;
   }
 
 }
