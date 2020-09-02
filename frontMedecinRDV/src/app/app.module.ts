1ce52bf578e083dd94c17db07ed2984ba83eb47f
SorelRouge <kevin.astier88@gmail.com>
2020-09-02 16:31:03 +0200

    [front] rajout de l'élément Register et formulaire médecin en cours

--- frontMedecinRDV/src/app/app.module.ts
+++ frontMedecinRDV/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RegisterModule } from './register/register.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RegisterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontMedecinRDV/src/app/register/form-medecin/form-medecin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-medecin',
+  templateUrl: './form-medecin.component.html',
+  styleUrls: ['./form-medecin.component.css']
+})
+export class FormMedecinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
