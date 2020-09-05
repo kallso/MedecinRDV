12acb2303732b93dc3ce489d88145aaf67ad57d9
Kevin C <costeskevin31@gmail.com>
2020-09-06 01:54:22 +0200

    [init] Export BDD apres renommage des ids en id_user partout et renommage codePostal en code_postal et idvRDV en idv_rdv

--- medecinrdv.sql
+++ medecinrdv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 31 août 2020 à 20:06
+-- Généré le :  sam. 05 sep. 2020 à 23:00
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -33,11 +33,11 @@ USE `medecinrdv`;
 DROP TABLE IF EXISTS `creneau`;
 CREATE TABLE IF NOT EXISTS `creneau` (
   `id_creneau` int(11) NOT NULL AUTO_INCREMENT,
-  `id_medecin` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `date_debut` datetime NOT NULL,
   `date_fin` datetime NOT NULL,
   PRIMARY KEY (`id_creneau`),
-  KEY `creneau_medecin` (`id_medecin`)
+  KEY `creneau_medecin` (`id_user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -48,14 +48,14 @@ CREATE TABLE IF NOT EXISTS `creneau` (
 
 DROP TABLE IF EXISTS `medecin`;
 CREATE TABLE IF NOT EXISTS `medecin` (
-  `id_medecin` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `specialisation` varchar(255) NOT NULL,
   `adresse` varchar(255) NOT NULL,
   `tempsRDV` int(11) NOT NULL,
   `codePostal` int(11) NOT NULL,
   `ville` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_medecin`),
-  UNIQUE KEY `id_medecin` (`id_medecin`)
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `id_medecin` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -66,17 +66,17 @@ CREATE TABLE IF NOT EXISTS `medecin` (
 
 DROP TABLE IF EXISTS `patient`;
 CREATE TABLE IF NOT EXISTS `patient` (
-  `id_patient` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `num_secu` int(11) NOT NULL,
-  PRIMARY KEY (`id_patient`),
-  KEY `id_patient` (`id_patient`)
+  PRIMARY KEY (`id_user`),
+  KEY `id_patient` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `patient`
 --
 
-INSERT INTO `patient` (`id_patient`, `num_secu`) VALUES
+INSERT INTO `patient` (`id_user`, `num_secu`) VALUES
 (2, 12345);
 
 -- --------------------------------------------------------
@@ -87,12 +87,12 @@ INSERT INTO `patient` (`id_patient`, `num_secu`) VALUES
 
 DROP TABLE IF EXISTS `rdv`;
 CREATE TABLE IF NOT EXISTS `rdv` (
-  `id_patient` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `id_creneau` int(11) NOT NULL,
   `heure_debut` datetime NOT NULL,
   `id_rdv` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id_rdv`),
-  KEY `rdv_patient` (`id_patient`),
+  KEY `rdv_patient` (`id_user`),
   KEY `rdv_crenau` (`id_creneau`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -112,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `telephone` varchar(255) NOT NULL,
   PRIMARY KEY (`id_user`),
   KEY `id_user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
@@ -121,7 +121,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id_user`, `nom`, `prenom`, `mail`, `password`, `telephone`) VALUES
 (1, 'Raiff', 'Bertrand', 'bertrand@VM.com', 'LAVMMARCHEPA', '+33666666666'),
 (2, 'Garcia', 'Simon', 'fantome@gmail.com', 'jesuispalaaaaa', '+33541235822'),
-(3, 'Balatia', 'Yaya', 'yayaToure@but.com', 'CLEBUUUUUUT', '+33544658498');
+(3, 'Balatia', 'Yaya', 'yayaToure@but.com', 'CLEBUUUUUUT', '+33544658499'),
+(8, 'Ulster', 'Francis', 'franfran@casonade.slah', '123456789', '+3366666666');
 
 --
 -- Contraintes pour les tables déchargées
@@ -131,26 +132,26 @@ INSERT INTO `user` (`id_user`, `nom`, `prenom`, `mail`, `password`, `telephone`)
 -- Contraintes pour la table `creneau`
 --
 ALTER TABLE `creneau`
-  ADD CONSTRAINT `creneau_medecin` FOREIGN KEY (`id_medecin`) REFERENCES `medecin` (`id_medecin`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `creneau_medecin` FOREIGN KEY (`id_user`) REFERENCES `medecin` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `medecin`
 --
 ALTER TABLE `medecin`
-  ADD CONSTRAINT `user_medecin` FOREIGN KEY (`id_medecin`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_medecin` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `patient`
 --
 ALTER TABLE `patient`
-  ADD CONSTRAINT `user_patient` FOREIGN KEY (`id_patient`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_patient` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `rdv`
 --
 ALTER TABLE `rdv`
   ADD CONSTRAINT `rdv_creneau` FOREIGN KEY (`id_creneau`) REFERENCES `creneau` (`id_creneau`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `rdv_patient` FOREIGN KEY (`id_patient`) REFERENCES `patient` (`id_patient`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `rdv_patient` FOREIGN KEY (`id_user`) REFERENCES `patient` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
